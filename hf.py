923683420cf49085d0b2ce9fe00645d0a76e8bb2
sskamuju <sakamuju@gmail.com>
2025-05-13 11:35:07 -0700

    Create hf.py

---
+++ hf.py
@@ -0,0 +1,9 @@
+# Load model directly
+
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+
